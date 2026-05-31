1cd808144cfff6d2fded9922cc1b52b7c4ee9fbe
mababama21 <2425402893@live.gctu.edu.gh>
2026-05-31 13:19:11 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: This program prints out my initials (AYE) in a large, stylized format.
+# Assignment Number: 1
+#
+# Name: Abraham Essuman
+# STUDENT ID: 2425402893
+# Email: essumanabraham715@gmail.com
+# Grader: Augustus Backman
+#
+# On my honor, Abraham Essuman, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function handles the printing of my initials. I've designed each letter 
+    # to be 12x10 and added the asterisk periods as required.
+    print()
+    print("...AYE")
+    print()
+    print("...    AAAA    ...  ...YY        YY...  ...EEEEEEEEEEEE...  ")
+    print("...   AA  AA   ...  ... YY      YY ...  ...EE          ...  ")
+    print("...  AA    AA  ...  ...  YY    YY  ...  ...EE          ...  ")
+    print("... AA      AA ...  ...   YY  YY   ...  ...EE          ...  ")
+    print("...AA        AA...  ...    YYYY    ...  ...EEEEEEEEEEEE...  ")
+    print("...AAAAAAAAAAAA...  ...     YY     ...  ...EE          ...  ")
+    print("...AA        AA...  ...     YY     ...  ...EE          ...  ")
+    print("...AA        AA...  ...     YY     ...  ...EE          ...  ")
+    print("...AA        AA...**...     YY     ...**...EE          ...**")
+    print("...AA        AA...**...     YY     ...**...EEEEEEEEEEEE...**")
+    print()
 
 
 main()
